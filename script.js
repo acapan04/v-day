1d866ea5863d00953812b6c217c45864efd5dc90
acap25 <156203390+acapan04@users.noreply.github.com>
2026-02-14 06:21:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    "https://media.tenor.com/hRR50Ijq2jkAAAAi/cartoon-network-escandalosos.gif",    // 0 normal
+    "https://media1.tenor.com/m/vkaCbh3fKiQAAAAC/confused-panda.gif",  // 1 confused
+    "https://media1.tenor.com/m/Ufj5FLC2cY0AAAAC/please-baby-grizzly.gif",             // 2 pleading
+    "https://media1.tenor.com/m/22hwAGkxTDEAAAAC/we-bare-bears-sad.gif",             // 3 sad
+    "https://media.tenor.com/dUaOCPatbs8AAAAi/we-bare-bears-cry.gif",       // 4 sadder
+    "https://media1.tenor.com/m/FMK8HlQRUosAAAAC/milk-bear.gif",             // 5 devastated
+    "https://media1.tenor.com/m/74PlfOXmxIMAAAAC/phuoctino.gif",               // 6 very devastated
+    "https://media1.tenor.com/m/VHugmWVSGW4AAAAC/yay-we-bear-bears.gif"  // 7 crying runaway
 ]
 
 const noMessages = [
     "No",
-    "Are you positive? 🤔",
-    "Pookie please... 🥺",
+    "Are you really sure? 🤔",
+    "Please my baby boo... 🥺",
     "If you say no, I will be really sad...",
     "I will be very sad... 😢",
-    "Please??? 💔",
-    "Don't do this to me...",
-    "Last chance! 😭",
-    "You can't catch me anyway 😜"
+    "Please my love??? 💔",
+    "Hmmm Don't do this to me...",
+    "Last chance pweaseee! 😭",
+    "You can't catch me anyway huhu 😜"
 ]
 
 const yesTeasePokes = [
